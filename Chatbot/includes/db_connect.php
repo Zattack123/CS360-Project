882c43853d102c46174fa726d7dbbf5960b8fb71
ZachDilliha <zdilliha@gmail.com>
2020-10-30 18:20:17 -0500

    Added test php and mysql connect file

--- Chatbot/includes/db_connect.php
+++ Chatbot/includes/db_connect.php
@@ -8,6 +8,7 @@ $password = "root";
 $dbName = "chatbot";
 
 $conn = mysqli_connect($servername, $username, $password, $dbName);
+echo "This is working?";
 
 if(!$conn){
   die("Connection Failed: ".mysqli_connect_error());
