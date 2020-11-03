57b1050b4fe85d4aaa3aa12271dc709ecd011b28
ZachDilliha <zdilliha@gmail.com>
2020-11-02 18:19:41 -0600

    ?

--- Chatbot/includes/db_connect.php
+++ Chatbot/includes/db_connect.php
@@ -3,7 +3,7 @@ session_start();
 $_SESSION['testVar'] = "Test";
 
 
-$servername = "localhost:3306";
+$servername = "99.82.204.102:3306";
 $username = "root";
 $password = "root";
 $dbName = "chatbot";
