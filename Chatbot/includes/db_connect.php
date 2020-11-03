4af1a150abfd5f2ecb7ecb447d4fcf8d6a967ac6
ZachDilliha <zdilliha@gmail.com>
2020-11-02 21:01:01 -0600

    GRhty

--- Chatbot/includes/db_connect.php
+++ Chatbot/includes/db_connect.php
@@ -10,6 +10,12 @@ $dbName = "chatbot";
 
 $conn = mysqli_connect($servername, $username, $password, $dbName, "3306");
 echo "Is this working?";
+if (mysqli_ping($conn)) {
+    echo "Our connection is ok!\n";
+} else {
+    echo "Error: %s\n", mysqli_error($link);
+}
+
 
 if(!$conn){
   die("Connection Failed: ".mysqli_connect_error());
