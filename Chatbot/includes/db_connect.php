4850ea4ef448ee2d8f00fa5841a7a78ac2cda33a
ZachDilliha <zdilliha@gmail.com>
2020-11-02 21:05:05 -0600

    error

--- Chatbot/includes/db_connect.php
+++ Chatbot/includes/db_connect.php
@@ -15,7 +15,9 @@ echo "Is this working?";
 if (mysqli_ping($conn)) {
     printf ("Our connection is ok!\n");
 } else {
-    printf ("Error: %s\n", mysqli_error($link));
+  echo "Error: Unable to connect to MySQL." . PHP_EOL;
+  echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+  echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
 }
 
 
