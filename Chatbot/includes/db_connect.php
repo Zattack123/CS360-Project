86495f3b4f5317c9b74c50c7aea64e01d57578ba
ZachDilliha <zdilliha@gmail.com>
2020-11-04 21:11:15 -0600

    waEgr

--- Chatbot/includes/db_connect.php
+++ Chatbot/includes/db_connect.php
@@ -4,8 +4,8 @@ $_SESSION['testVar'] = "Test";
 
 
 $servername = "db_container";
-$username = "root";
-$password = "newpassword";
+$username = "zach";
+$password = "zach";
 $dbName = "chatbot";
 
 $conn = mysqli_connect($servername, $username, $password, $dbName);
